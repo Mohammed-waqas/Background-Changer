a5a489914b35ae6ab33efcb0915452c389a02463
waqas-engineer <waqasengineer9733@gmail.com>
2024-04-23 09:41:50 +0500

    create a navbar with click button

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,49 @@ function App() {
         style={{ backgroundColor: color }}
       >
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white ">
-            test
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button
+              onClick={() => setColor("red")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "red" }}
+            >
+              Red
+            </button>
+            <button
+              onClick={() => setColor("green")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "green" }}
+            >
+              Green
+            </button>
+            <button
+              onClick={() => setColor("blue")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "blue" }}
+            >
+              Blue
+            </button>
+            <button
+              onClick={() => setColor("purple")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "purple" }}
+            >
+              Purple
+            </button>
+            <button
+              onClick={() => setColor("crimson")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "crimson" }}
+            >
+              Crimson
+            </button>
+            <button
+              onClick={() => setColor("brown")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "brown" }}
+            >
+              Brown
+            </button>
           </div>
         </div>
       </div>
